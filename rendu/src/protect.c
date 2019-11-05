4a40a96e8e384de71e173178374247d9fa2e9ea7
alagache <alagache@student.42.fr>
2019-11-05 15:49:37 +0100

    protection OK

--- rendu/src/protect.c
+++ rendu/src/protect.c
@@ -6,25 +6,57 @@
 /*   By: plagache <plagache@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 12:40:12 by plagache          #+#    #+#             */
-/*   Updated: 2019/11/05 14:59:39 by plagache         ###   ########.fr       */
+/*   Updated: 2019/11/05 15:49:04 by alagache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-/*
-int		verification(int fd, t_map *map, t_list *head)
+
+int		check_ints(char *str)
 {
+	int	c;
 
+	c = 0;
+	while (str[c] && c < 6 && ft_isdigit(str[c]) == 1)
+		c++;
+	if (c == 6)
+		return (-1);
+	return (c);
 }
-*/
 
-int		is_correct(char *str)
+int		loop_ints(int fd)
+{
+	int		ret;
+	int		c;
+	char	*line;
+
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		c = 0;
+		while (line[c])
+		{
+			ret = 0;
+			if (ft_isdigit(line[c]) == 1 &&  (ret = check_ints(line + c)) == -1)
+			{
+				free(line);
+				return (-1);
+			}
+			c += ret;
+			while (line[c] && line[c] == ' ')
+				c++;
+		}
+		free(line);
+	}
+	return (0);
+}
+
+int		is_correct(char *str, int len)
 {
 	int c;
 
 	c = 0;
-	while (str[c])
+	while (c < len)
 	{
 		if (ft_isdigit(str[c]) != 1 && str[c] != ' ' && str[c] != '-' && str[c] != '\n')
 			return (0);
@@ -41,7 +73,7 @@ int		first_read(int fd)
 	while ((ret = read(fd, buff, 100)) > 0)
 	{
 		buff[ret] = '\0';
-		if (is_correct(&buff[0]) == 0)
+		if (is_correct(&buff[0], ret) == 0)
 			return (-1);
 	}
 	return (0);
@@ -52,25 +84,22 @@ int		protect(char *path)
 	int		fd;
 	char	buff[1];
 
-	if (ft_strnstr(path, "/dev", 4))
-		return (-1);
-	if (ft_strstr(path + ft_strlen(path) - 4, ".fdf") == NULL)
+	if (ft_strnstr(path, "/dev", 4)
+			|| ft_strstr(path + ft_strlen(path) - 4, ".fdf") == NULL)
 		return (-1);
 	fd = open(path, O_RDONLY);
-	if (fd < 0)
+	if (fd < 0 || read(fd, buff, 1) < 0 || first_read(fd) == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	if (read(fd, buff, 1) < 0)
+	close(fd);
+	fd = open(path, O_RDONLY);
+	if (loop_ints(fd) == -1)
 	{
 		close(fd);
-		return (-1);
+		return (-1);	
 	}
-	if (first_read(fd) == -1)
-	{
-		close(fd);
-		return (-1);
-	}
+	close(fd);
 	return (0);
 }
